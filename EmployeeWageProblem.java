389a48296f8758f90b040e9bab8e46f27d8f0541
Akshay Barve <akshaybarve06@gmail.com>
2020-03-19 18:31:20 +0530

    Added: Employee Wage For Day

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,12 +1,24 @@
+// Description : Employee Wage Problem
+// Author : Akshay Dhananjay Barve
+// Version : 18.04.3 lts
+// Since 19th March 2020 / Thursday
+
 public class EmployeeWageProblem
 {
 	public static void main ( String args[] )
 	{
 		int checkNumber=(int)(Math.floor(Math.random()*10)%2);
-		int IS_PRESENT=1;
+		int salary=0, IS_PRESENT=1,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8;
 		if ( checkNumber == IS_PRESENT )
-			System.out.println("Employee Is Present..!");
+		{
+         System.out.println("Employee Is Present..!");
+			salary=(WAGE_PER_HOUR*FULL_DAY_HOUR);
+			System.out.println("Employee Salary = "+salary);
+		}
 		else
+		{
 			System.out.println("Employee Is Absent..!");
+	      System.out.println("Employee Salary = "+salary);
+		}
 	}
 }
