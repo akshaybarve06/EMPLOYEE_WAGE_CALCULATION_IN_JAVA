8437484ec03c8a44edac4bf7d8c45ed1138605ca
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-20 14:13:40 +0530

    Modified: Find Employee Wage For Month

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -8,7 +8,7 @@ public class EmployeeWageProblem
       public static void main ( String args[] )
       {
       final int IS_FULLTIME=1, IS_PARTTIME=2, NOOFDAYS=20, WAGE_PER_HOUR=20;
-      int totalSalary=0
+      int totalSalary=0;
       for ( int i=0; i<NOOFDAYS; i++ )
       {
          int checkNumber=(int)(Math.floor(Math.random()*10)%3);
@@ -16,22 +16,19 @@ public class EmployeeWageProblem
          switch (checkNumber)
          {
          case IS_FULLTIME:
-               System.out.println("Employee Is Full Time..!");
                empHours=8;
                break;
          case IS_PARTTIME:
-               System.out.println("Employee Is Part Time..!");
                empHours=4;
                break;
          default:
-               System.out.println("Employee Is Absent..");
                empHours=0;
                break;
          }
             salary=empHours*WAGE_PER_HOUR;
-            System.out.println("Salary of Employee is.."+salary);
+            totalSalary+=salary;
+            System.out.println("Salary of Employee on "+i+ " day is.."+salary);
       }
-      totalSalary+=salary;
       System.out.println("ToTal Salary of Employee is.."+ totalSalary);
    }
 }
