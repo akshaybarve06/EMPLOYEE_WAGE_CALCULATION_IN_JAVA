ba58bc21572065b3dc594ac7a16587090c588b6f
Akshay Barve <akshaybarve06@gmail.com>
2020-03-20 11:50:53 +0530

    Added: Part Time Employee Wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -7,18 +7,26 @@ public class EmployeeWageProblem
 {
 	public static void main ( String args[] )
 	{
-		int checkNumber=(int)(Math.floor(Math.random()*10)%2);
-		int salary=0, IS_PRESENT=1,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8;
-		if ( checkNumber == IS_PRESENT )
+		int checkNumber=(int)(Math.floor(Math.random()*10)%3);
+		int salary=0, empHours=0;
+		int IS_FULLTIME=1, IS_PARTTIME=2, WAGE_PER_HOUR=20;
+		if ( checkNumber == IS_FULLTIME )
 		{
-         		System.out.println("Employee Is Present..!");
-			salary=(WAGE_PER_HOUR*FULL_DAY_HOUR);
-			System.out.println("Employee Salary = "+salary);
+			System.out.println("Employee Is Full Time..!");
+			empHours=8;
 		}
 		else
+		if ( checkNumber == IS_PARTTIME )
+      {
+         System.out.println("Employee Is Part Time..!");
+         empHours=4;
+      }
+		else
 		{
-			System.out.println("Employee Is Absent..!");
-	      		System.out.println("Employee Salary = "+salary);
+			System.out.println("Employee Is Absent..");
+			empHours=0;
 		}
+		salary=empHours*WAGE_PER_HOUR;
+		System.out.println("Salary of Employee is.."+salary);
 	}
 }
