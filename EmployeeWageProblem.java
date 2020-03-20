827b2dd2d362b8a3364e209a8b98d2145df05bd8
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-20 11:08:46 +0530

    Modified: Employee Wage For Day

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -11,14 +11,14 @@ public class EmployeeWageProblem
 		int salary=0, IS_PRESENT=1,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8;
 		if ( checkNumber == IS_PRESENT )
 		{
-         System.out.println("Employee Is Present..!");
+         		System.out.println("Employee Is Present..!");
 			salary=(WAGE_PER_HOUR*FULL_DAY_HOUR);
 			System.out.println("Employee Salary = "+salary);
 		}
 		else
 		{
 			System.out.println("Employee Is Absent..!");
-	      System.out.println("Employee Salary = "+salary);
+	      		System.out.println("Employee Salary = "+salary);
 		}
 	}
 }
