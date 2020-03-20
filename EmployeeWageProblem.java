c3606cc785d43419ce14e9e7c58733a469c36411
Akshay Barve <akshaybarve06@gmail.com>
2020-03-20 17:55:11 +0530

    Added: Wage Calculation Upto Given Limit

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -5,33 +5,32 @@
 
 public class EmployeeWageProblem
 {
-	public static void main ( String args[] )
-   {
-      final int IS_FULLTIME=1, IS_PARTTIME=2, NOOFDAYS=20; WAGE_PER_HOUR=20;
-      int totalSalary=0
-      for ( int i=0; i<NOOFDAYS; i++ )
+   public static void main ( String args[] )
+    {
+      final int IS_FULLTIME=1, IS_PARTTIME=2, MAX_DAYS=20, WAGE_PER_HOUR=20, MAX_HOURS=100;
+      int totalSalary=0, totalEmpHours=0, noOfDays=0;
+      while ( totalEmpHours < MAX_HOURS && noOfDays < MAX_DAYS )
       {
-         int checkNumber=(int)(Math.floor(Math.random()*10)%3);
-         int salary=0, empHours=0;
-         switch (checkNumber)
-         {
-         case IS_FULLTIME:
-               System.out.println("Employee Is Full Time..!");
-               empHours=8;
-               break;
-         case IS_PARTTIME:
-               System.out.println("Employee Is Part Time..!");
-               empHours=4;
-               break;
-         default:
-               System.out.println("Employee Is Absent..");
-               empHours=0;
-               break;
-         }
-            salary=empHours*WAGE_PER_HOUR;
-            System.out.println("Salary of Employee is.."+salary);
+        int checkNumber=(int)(Math.floor(Math.random()*10)%3);
+        noOfDays+=1;
+        int salary=0, empHours=0;
+        switch (checkNumber)
+        {
+        case IS_FULLTIME:
+            empHours=8;
+            break;
+        case IS_PARTTIME:
+            empHours=4;
+            break;
+        default:
+            empHours=0;
+            break;
+        }
+        salary=empHours*WAGE_PER_HOUR;
+        totalEmpHours+=empHours;
+        totalSalary+=salary;
+        System.out.println("Salary of Employee on "+noOfDays+ "th Day is.."+salary);
       }
-      totalSalary+=salary;
       System.out.println("ToTal Salary of Employee is.."+ totalSalary);
    }
 }
